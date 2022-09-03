03ac1b5c87082d73098144108422f9bb8eeef739
Keitaro-Ninomiya <67201847+Keitaro-Ninomiya@users.noreply.github.com>
2022-09-03 14:10:54 -0400

    First Commit

---
+++ Code.R
@@ -0,0 +1,5 @@
+RagnarPath="/home/keitaro2/Uber_Chicago/Uber_Chicago-Git"
+LocalPath="C:/Users/Keitaro Ninomiya/Box/Research Notes (keitaro2@illinois.edu)/Uber_Chicago"
+
+#Pull most recent Github version.
+library(usethis)
