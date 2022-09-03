06ac92b03314500756ed7e4b19d8d20f87e78dcd
Keitaro-Ninomiya <67201847+Keitaro-Ninomiya@users.noreply.github.com>
2022-09-03 17:14:24 -0400

    First Commit

--- Code.R
+++ Code.R
@@ -3,3 +3,10 @@ LocalPath="C:/Users/Keitaro Ninomiya/Box/Research Notes (keitaro2@illinois.edu)/
 
 #Pull most recent Github version.
 library(usethis)
+gh_token_help()
+
+create_from_github("https://github.com/Keitaro-Ninomiya/Uber_Chicago.git")
+gitcreds::gitcreds_set() 
+"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"
+
+#
