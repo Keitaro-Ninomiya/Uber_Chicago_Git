12f84ae949e05ce583b0e54c56f6d4de113675ae
Keitaro-Ninomiya <67201847+Keitaro-Ninomiya@users.noreply.github.com>
2022-09-15 10:48:37 -0500

    Changed main specification to boundaries with clean separation

---
+++ Results/DID/Regression/Code.R
@@ -0,0 +1,28 @@
+reg=function(Dist,Char){
+  results = fepois(Count~GeoTrt:Post+PairDist|OD_Pair+as_factor(week)+Post,
+                   data=dtaDay1 %>% 
+                     filter(PU_Dist<Dist|DO_Dist<Dist))
+  return(results)
+}
+
+regPD=function(Dist,Char){
+  results = fepois(Count~GeoTrt:Post+PairDist|as_factor(PU_Tract)+as_factor(DO_Tract)+as_factor(week)+Post,
+                   data=dtaDay1 %>% 
+                     filter(PU_Dist<Dist|DO_Dist<Dist))
+  return(results)
+}
+
+regBD=function(dta,Dist){
+  results = glm(Count~GeoTrt+as_factor(week)+GeoTrt:Post+Post+PairDist,
+                data=dta %>% 
+                  filter(PU_Dist<Dist|DO_Dist<Dist,
+                         PU_BdN==1|DO_BdN==1|PU_BdS==1|DO_BdS==1),
+                family = poisson(link = "log"))
+  return(results)
+}
+
+report=function(results,Char){
+  sink(Char)
+  summary(results)
+  sink()
+}
