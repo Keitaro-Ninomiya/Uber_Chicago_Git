5b73061894c40700905567ce8a7e4690d4744e95
Keitaro-Ninomiya <67201847+Keitaro-Ninomiya@users.noreply.github.com>
2022-10-10 21:59:21 -0500

    Separated DID from DIDID codes.

--- Results/DID/Code.R
+++ Results/DID/Code.R
@@ -11,35 +11,16 @@ path_remote_OL="/home/keitaro2/Uber_Chicago_OL"
 setwd(paste(path_remote_OL,"/Results/DID",sep=""))
 
 #Geo####
+source(paste(path_remote,"Results/DID/Regression/Code.R",sep=""))
 dtaDay1 = read.csv("/home/keitaro2/Uber_Chicago/Raw_Data/Key_Data/dtaDay1.csv")
 dtaDay1=dtaDay1 %>% 
   group_by(OD_Pair,Date) %>% 
   mutate(ID = cur_group_id())
 
-#Int
-dta15=read.csv("/home/keitaro2/Uber_Chicago/Raw_Data/Key_Data/dtaInt15Month_v2.csv")
-dta15 = dta15 %>% 
-  mutate(IntTrt = case_when(Hour%in%c(5,22)~1,
-                            TRUE~0),
-         Time=case_when(Hour%in%c(5,6)~"Morning",
-                        TRUE~"Evening"))
-
 #Main Results ####
-#Morning
-source("Regression/Code.R")
-Result=regBD_Int(dta15,"Morning")
-fileConn<-file("Results_Int_Morning.tex")
-writeLines(Result, fileConn)
-close(fileConn)
-
-#Evening
-Result=regBD_Int(dta15,"Evening")
-fileConn<-file("Results_Int_Evening.tex")
-writeLines(Result, fileConn)
-close(fileConn)
-
-#Pooled
-Result=regBD_Int(dta15,"Both")
-fileConn<-file("Results_Int.tex")
+#DID####
+#DID####
+Result=regBD(dtaDay1)
+fileConn<-file("Results_Geo.tex")
 writeLines(Result, fileConn)
 close(fileConn)
